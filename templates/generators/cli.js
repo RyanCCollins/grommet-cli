4954a936b2e65f63dbb7a89a9805ac2aeb0faa32
Ryan Collins <rccproductions@mac.com>
2017-01-10 22:49:18 -0500

    Feat: implement container generators

--- templates/generators/cli.js
+++ templates/generators/cli.js
@@ -1,11 +1,16 @@
 const componentGenerator = require('./component/index.js');
-// const containerGenerator = require('./container/index.js');
+const SafeString = require('./utils/safeString').SafeString;
+const containerGenerator = require('./container/index.js');
 
 module.exports = (plop) => {
   plop.setGenerator('component', componentGenerator);
-  // plop.setGenerator('container', containerGenerator);
+  plop.setGenerator('container', containerGenerator);
   plop.addHelper('uppercase', (text) => {
     return text.toUpperCase();
+  });
+  plop.addHelper('createImports', (list) => {
+    const items = list.map((item) => `import ${item} from 'grommet/components/${item}';`).join('\n');
+    return new SafeString(items);
   });
   plop.addHelper('curly', (object, open) => (open ? '{' : '}'));
 };
--- templates/generators/component/index.js
+++ templates/generators/component/index.js
@@ -13,7 +13,7 @@ module.exports = {
     {
       type: 'input',
       name: 'path',
-      message: 'Where should we put the components (relative to root)?',
+      message: 'What directory would you like your component in? (relative)',
       default: './src/js/components',
       validate: (value) => {
         return true;
@@ -30,6 +30,22 @@ module.exports = {
         }
         return 'The name is required.';
       }
+    },
+    {
+      type: 'checkbox',
+      name: 'imports',
+      message: 'Would you like to import any commonly used grommet components?',
+      choices: () => [
+        { name: 'Anchor', value: 'Anchor', checked: false },
+        { name: 'Article', value: 'Article', checked: false },
+        { name: 'Button', value: 'Button', checked: false },
+        { name: 'Card', value: 'Card', checked: false },
+        { name: 'Heading', value: 'Heading', checked: false },
+        { name: 'Header', value: 'Header', checked: false },
+        { name: 'Footer', value: 'Footer', checked: false },
+        { name: 'Paragraph', value: 'Paragraph', checked: false },
+        { name: 'Section', value: 'Section', checked: false }
+      ]
     },
     {
       type: 'confirm',
@@ -62,7 +78,7 @@ module.exports = {
     if (data.wantJestTests) {
       actions.push({
         type: 'add',
-        path: `${componentPath}/index.test.js`,
+        path: `${componentPath}/tests/index.test.js`,
         templateFile: './component/test.js.hbs',
         abortOnFail: true
       });
